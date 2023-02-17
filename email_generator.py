85bb97bdb901578865990cf8b97ca9e50f81e664
KAT <104945136+KATT07@users.noreply.github.com>
2023-02-17 22:21:42 +0530

    Delete email_generator.py

--- email_generator.py
+++
@@ -1,9 +0,0 @@
-import random
-mail_l=["@gmail.com","@gmail.com","@yahoo.com","@hotmail.com"]
-f=open("emp_names_20000.txt","r")
-g=open("email_names.txt","w")
-fname_l=f.readlines()
-for i in fname_l:
-    st=i.split()
-    e_mail=st[0]+'.'+st[1]+mail_l[random.randint(0,3)]
-    g.write(e_mail+"\n")
